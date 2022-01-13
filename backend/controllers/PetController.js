2fb49013c617da10d826ce2e6e2075b292394a6c
Felipe118 <lipebezerraa@gmail.com>
2022-01-13 20:39:51 -0300

    Funcionalidades dos pets

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -1,6 +1,7 @@
 const getToken = require("../helpers/get-token.js")
 const getUserByToken = require("../helpers/get-user-by-token.js")
 const Pet = require("../models/Pet.js")
+const ObjectId = require("mongoose").Types.ObjectId
 
 module.exports = class PetController{
     //create a pet
@@ -91,5 +92,36 @@ module.exports = class PetController{
         res.status(200).json({
             pets,
         })
+    }
+
+    static async getAllUserAdoprions(req,res){
+        const token = getToken(req)
+        const user = await getUserByToken(token)
+
+        const pets = await Pet.find({'adopter._id':user._id}).sort('-createdAt')
+        res.status(200).json({
+            pets,
+        })
+    }
+
+    static async getPetById(req,res){
+           const id = req.params.id 
+
+        if(!ObjectId.isValid(id)){
+            res.status(422).json({message: "ID inválido"})
+            return
+        }
+
+        const pet = await Pet.findOne({_id:id })
+
+        if(!pet){
+            res.status(404).json({message: "Pet não encontrado"})
+            return
+        }
+        res.status(200).json({
+            pet: pet,
+        })
+
+          
     }
 }
