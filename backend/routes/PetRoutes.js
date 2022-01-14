662feb8689ad7ea8488a40ed7218d5eb52977467
Felipe118 <lipebezerraa@gmail.com>
2022-01-13 21:48:31 -0300

    Concluindo o agendamento de visita dos pets

--- backend/routes/PetRoutes.js
+++ backend/routes/PetRoutes.js
@@ -12,6 +12,8 @@ router.get('/mypets', verifyToken,PetController.getAllUserPets )
 router.get('/myadoptions',verifyToken,PetController.getAllUserAdoprions)
 router.get('/:id',PetController.getPetById)
 router.delete('/:id',verifyToken,PetController.removePetById)
+router.patch('/:id', verifyToken,imageUpload.array('images'),PetController.updatePet)
+router.patch('/schedule/:id',verifyToken,PetController.schedule)
 
 module.exports = router
 
