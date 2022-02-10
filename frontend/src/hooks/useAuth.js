6bb48212c9711c02f96ada4572e5974f281c8564
Felipe118 <lipebezerraa@gmail.com>
2022-02-10 20:55:58 -0300

    Terminado metodo de registrar faltando implantar as flash messages

---
+++ frontend/src/hooks/useAuth.js
@@ -0,0 +1,22 @@
+// api
+import api from "../utils/api";
+
+import {useState,useEffect} from 'react'
+import {userHistory} from 'react'
+
+export default function useAuth(){
+    async function register(user){
+        try {
+            const data = await api.post('/users/register', user)
+            .then((response) => {
+                return response.data
+            })
+
+            console.log(data)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    return { register }
+}
+
