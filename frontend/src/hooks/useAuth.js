097c2b629790f746fd1350d1dc39f11a8e5b43c5
Felipe118 <lipebezerraa@gmail.com>
2022-02-10 22:07:41 -0300

    Concluido flash messages

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -3,9 +3,15 @@ import api from "../utils/api";
 
 import {useState,useEffect} from 'react'
 import {userHistory} from 'react'
+import useFlashMessage from './useFlashMessage'
 
 export default function useAuth(){
+    const {setFlashMessage} = useFlashMessage()
     async function register(user){
+
+        let msgText = 'Cadastro realizado com sucesso!'
+        let msgType = 'sucess'
+
         try {
             const data = await api.post('/users/register', user)
             .then((response) => {
@@ -14,8 +20,14 @@ export default function useAuth(){
 
             console.log(data)
         } catch (error) {
-            console.log(error)
+             msgText = error.response.data.message
+             msgType = 'error'
         }
+
+        setFlashMessage(msgText, msgType)
+        
+
+
     }
     return { register }
 }
